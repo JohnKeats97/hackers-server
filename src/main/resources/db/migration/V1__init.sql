fbc94bec0882797d56da566fc3b0acbe9a515058
Unknown <johnbuevich@yahoo.com>
2018-04-17 17:59:20 +0300

    changed db

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,9 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS intarray;
+
 CREATE TABLE IF NOT EXISTS users
 (
   email text NOT NULL,
   login text NOT NULL,
   password text NOT NULL,
   score integer NOT NULL DEFAULT 0,
+  tests INTEGER[] DEFAULT ARRAY[],
+  last_answer TIMESTAMP DEFAULT now(),
+  isEmail BOOLEAN DEFAULT false,
   CONSTRAINT unique_login PRIMARY KEY (login),
   CONSTRAINT unique_email UNIQUE (email)
 );
