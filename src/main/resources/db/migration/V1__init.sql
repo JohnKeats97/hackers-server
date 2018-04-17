9badd9aecd06e9074a1318f42167ab74440dd5d4
Unknown <johnbuevich@yahoo.com>
2018-04-17 18:56:11 +0300

    some

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users
   password text NOT NULL,
   score integer NOT NULL DEFAULT 0,
   tests INTEGER[] DEFAULT ARRAY[],
-  last_answer TIMESTAMP DEFAULT now(),
+  last_answer TIMESTAMP,
   isEmail BOOLEAN DEFAULT false,
   CONSTRAINT unique_login PRIMARY KEY (login),
   CONSTRAINT unique_email UNIQUE (email)
