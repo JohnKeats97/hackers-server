c21727dfe7fad413d87a3a17c8d4ffa0f1e5c079
Unknown <johnbuevich@yahoo.com>
2018-04-21 03:13:44 +0300

    some

--- src/main/java/seabattle/authorization/views/UserView.java
+++ src/main/java/seabattle/authorization/views/UserView.java
@@ -37,6 +37,13 @@ public final class UserView {
         this.score = score;
     }
 
+    public UserView(@JsonProperty("email") String email, @JsonProperty("login") String login,
+                    @JsonProperty("password") String password) {
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "login = " + login + " email = " + email + " password = " + password + " score = " + score;
